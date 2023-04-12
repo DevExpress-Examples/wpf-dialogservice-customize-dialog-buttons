010cab27b99f01022d99137ba13a22c373148592
DevExpressExampleBot <fakeEmail>
2023-04-12 18:38:45 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,14 +1,15 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 
 Namespace DXSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
--- VB/ViewModels/MainViewModel.vb
+++ VB/ViewModels/MainViewModel.vb
@@ -1,60 +1,68 @@
-﻿Imports System.Windows.Input
+Imports System.Windows.Input
 Imports DevExpress.Mvvm
 
 Namespace DXSample.ViewModels
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Private privateShowDialogCommand As ICommand
-		Public Property ShowDialogCommand() As ICommand
-			Get
-				Return privateShowDialogCommand
-			End Get
-			Private Set(ByVal value As ICommand)
-				privateShowDialogCommand = value
-			End Set
-		End Property
-		Private privateShowMessageBoxCommand As ICommand
-		Public Property ShowMessageBoxCommand() As ICommand
-			Get
-				Return privateShowMessageBoxCommand
-			End Get
-			Private Set(ByVal value As ICommand)
-				privateShowMessageBoxCommand = value
-			End Set
-		End Property
-		Protected ReadOnly Property MessageService() As IMessageBoxService
-			Get
-				Return GetService(Of IMessageBoxService)()
-			End Get
-		End Property
-		Protected ReadOnly Property DialogService() As IDialogService
-			Get
-				Return GetService(Of IDialogService)()
-			End Get
-		End Property
-		Private privateDialogViewModel As DialogViewModel
-		Protected Property DialogViewModel() As DialogViewModel
-			Get
-				Return privateDialogViewModel
-			End Get
-			Private Set(ByVal value As DialogViewModel)
-				privateDialogViewModel = value
-			End Set
-		End Property
+    Public Class MainViewModel
+        Inherits ViewModelBase
 
-		Public Sub New()
-			ShowMessageBoxCommand = New DelegateCommand(AddressOf OnShowMessageBoxCommandExecute)
-			ShowDialogCommand = New DelegateCommand(AddressOf OnShowDialogCommandExecute)
-			DialogViewModel = New DialogViewModel()
-		End Sub
+        Private _ShowDialogCommand As ICommand, _ShowMessageBoxCommand As ICommand, _DialogViewModel As DialogViewModel
 
-		Private Sub OnShowDialogCommandExecute()
-			Dim dialogResult As MessageResult = DialogService.ShowDialog(MessageButton.OKCancel, "DialogWindow", DialogViewModel)
-			MessageService.Show("Dialog Clicked Button: " & dialogResult)
-		End Sub
-		Private Sub OnShowMessageBoxCommandExecute()
-			MessageService.Show("This is a message box", "DXMessageBoxService", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information)
-		End Sub
-	End Class
+        Public Property ShowDialogCommand As ICommand
+            Get
+                Return _ShowDialogCommand
+            End Get
+
+            Private Set(ByVal value As ICommand)
+                _ShowDialogCommand = value
+            End Set
+        End Property
+
+        Public Property ShowMessageBoxCommand As ICommand
+            Get
+                Return _ShowMessageBoxCommand
+            End Get
+
+            Private Set(ByVal value As ICommand)
+                _ShowMessageBoxCommand = value
+            End Set
+        End Property
+
+        Protected ReadOnly Property MessageService As IMessageBoxService
+            Get
+                Return GetService(Of IMessageBoxService)()
+            End Get
+        End Property
+
+        Protected ReadOnly Property DialogService As IDialogService
+            Get
+                Return GetService(Of IDialogService)()
+            End Get
+        End Property
+
+        Protected Property DialogViewModel As DialogViewModel
+            Get
+                Return _DialogViewModel
+            End Get
+
+            Private Set(ByVal value As DialogViewModel)
+                _DialogViewModel = value
+            End Set
+        End Property
+
+        Public Sub New()
+            ShowMessageBoxCommand = New DelegateCommand(AddressOf OnShowMessageBoxCommandExecute)
+            ShowDialogCommand = New DelegateCommand(AddressOf OnShowDialogCommandExecute)
+            DialogViewModel = New DialogViewModel()
+        End Sub
+
+        Private Sub OnShowDialogCommandExecute()
+            Dim dialogResult As MessageResult = DialogService.ShowDialog(MessageButton.OKCancel, "DialogWindow", DialogViewModel)
+            MessageService.Show("Dialog Clicked Button: " & dialogResult)
+        End Sub
+
+        Private Sub OnShowMessageBoxCommandExecute()
+            MessageService.Show("This is a message box", "DXMessageBoxService", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information)
+        End Sub
+    End Class
 End Namespace
